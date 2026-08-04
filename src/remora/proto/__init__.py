5bfa0d63b868bfa7102d84a2c1aab0aef6a265ce
Ruoshi Li <iceboundrock@gmail.com>
2026-08-04 12:21:21 -0700

    core: re-export the generated core protocol classes from remora.proto Signed-off-by: Ruoshi Li <iceboundrock@gmail.com>

--- src/remora/proto/__init__.py
+++ src/remora/proto/__init__.py
@@ -1,9 +1,65 @@
-"""Protocol classes: hand-written seeds (issue #13) now, generated (issue #14) later."""
+"""Generated protocol classes — the issue-19 core set listed in codegen.toml."""
 
+from remora.proto.arp import ARP
+from remora.proto.dhcp import DHCP
+from remora.proto.dhcpv6 import DHCPV6
 from remora.proto.dns import DNS
 from remora.proto.eth import ETH
+from remora.proto.ftp import FTP
+from remora.proto.gre import GRE
+from remora.proto.http import HTTP
+from remora.proto.http2 import HTTP2
+from remora.proto.icmp import ICMP
+from remora.proto.icmpv6 import ICMPV6
+from remora.proto.igmp import IGMP
+from remora.proto.imap import IMAP
 from remora.proto.ip import IP
+from remora.proto.ipv6 import IPV6
+from remora.proto.llc import LLC
+from remora.proto.ntp import NTP
+from remora.proto.pop import POP
+from remora.proto.quic import QUIC
+from remora.proto.rtp import RTP
+from remora.proto.sctp import SCTP
+from remora.proto.sip import SIP
+from remora.proto.smtp import SMTP
+from remora.proto.snmp import SNMP
+from remora.proto.ssh import SSH
+from remora.proto.stp import STP
 from remora.proto.tcp import TCP
+from remora.proto.tls import TLS
 from remora.proto.udp import UDP
+from remora.proto.vlan import VLAN
 
-__all__ = ["DNS", "ETH", "IP", "TCP", "UDP"]
+__all__ = [
+    "ARP",
+    "DHCP",
+    "DHCPV6",
+    "DNS",
+    "ETH",
+    "FTP",
+    "GRE",
+    "HTTP",
+    "HTTP2",
+    "ICMP",
+    "ICMPV6",
+    "IGMP",
+    "IMAP",
+    "IP",
+    "IPV6",
+    "LLC",
+    "NTP",
+    "POP",
+    "QUIC",
+    "RTP",
+    "SCTP",
+    "SIP",
+    "SMTP",
+    "SNMP",
+    "SSH",
+    "STP",
+    "TCP",
+    "TLS",
+    "UDP",
+    "VLAN",
+]
